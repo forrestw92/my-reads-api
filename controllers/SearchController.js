d1186a1ee2b50f5d23c6b1cbb9c693957ad2be8a
Forrest Walker <me@forrestwalker.net>
2018-08-29 17:59:00 -0500

    Fixed options in search query.

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -2,11 +2,16 @@ const {defaultOptions} = require('../config')
 const books = require('../api')
 const Book = require('../models/BookModel')
 class SearchController {
-  constructor () {
-    this.searchType = ['books', 'magazines', 'all']
-    this.orderTypes = ['relevance', 'newest']
-    this.fieldTypes = ['title', 'author', 'publisher', 'subject', 'isbn']
-    this.maxLimit = 40
+  /**
+     * Sets allowed options
+     */
+  static getAllowedOptions () {
+    return {
+      searchType: ['books', 'magazines', 'all'],
+      orderTypes: ['relevance', 'newest'],
+      fieldTypes: ['title', 'author', 'publisher', 'subject', 'isbn'],
+      maxLimit: 40
+    }
   }
   /**
      * Gets results from search query
@@ -19,18 +24,19 @@ class SearchController {
     const token = req.token
     if (!query) return res.status(400).json({error: 'INVALID_QUERY'})
     if (options) {
+      if (options.key) delete options.key
       if (options.limit && !Number.isInteger(options.limit)) return res.status(400).json({error: 'LIMIT_NOT_VALID'})
       if (options.startIndex && !Number.isInteger(options.startIndex)) return res.status(400).json({error: 'INDEX_NOT_VALID'})
       if (options.limit < 1) return res.status(400).json({error: 'MIN_LIMIT_1'})
-      if (options.limit > this.maxLimit) return res.status(400).json({error: 'MAX_LIMIT_40'})
+      if (options.limit > SearchController.getAllowedOptions().maxLimit) return res.status(400).json({error: 'MAX_LIMIT_40'})
       if (options.startIndex < 1) return res.status(400).json({error: 'MIN_LIMIT_1'})
 
-      if (options.field && !this.fieldTypes.includes(options.field)) return res.status(400).json({error: 'INVALID_FIELD_TYPE'})
-      if (options.orderBy && !this.orderTypes.includes(options.orderBy)) return res.status(400).json({error: 'INVALID_ORDER_BY_TYPE'})
-      if (options.resultType && !this.searchType.includes(options.searchType)) return res.status(400).json({error: 'INVALID_RESULT_TYPE'})
+      if (options.field && !SearchController.getAllowedOptions().fieldTypes.includes(options.field)) return res.status(400).json({error: 'INVALID_FIELD_TYPE'})
+      if (options.orderBy && !SearchController.getAllowedOptions().orderTypes.includes(options.orderBy)) return res.status(400).json({error: 'INVALID_ORDER_BY_TYPE'})
+      if (options.resultType && !SearchController.getAllowedOptions().searchType.includes(options.searchType)) return res.status(400).json({error: 'INVALID_RESULT_TYPE'})
     }
     try {
-      let bookResults = await books.search(query, options || defaultOptions)
+      let bookResults = await books.search(query, Object.assign(defaultOptions, options))
       if (bookResults.length === 0) {
         return res.status(400).json({error: 'NO_RESULTS'})
       }
